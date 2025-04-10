1b5d8e77f9665069e85b5d839deba88f8e5de8be
RodenLuo <roden.luo@gmail.com>
2025-04-10 12:39:33 +0300

    Enhance AF2igImport module to set graph name during import

--- plugins/proteincraft/import/AF2igImport.cpp
+++ plugins/proteincraft/import/AF2igImport.cpp
@@ -19,32 +19,33 @@ using namespace tlp;
 class AF2igImport : public ImportModule {
 public:
 
-  // This line is used to pass information about the current plug-in.
-  PLUGININFORMATION("AF2igImport",
-                    "Roden Luo",
-                    "2025/04/10",
-                    "Import AF2ig files",
-                    "1.0",
-                    "ProteinCraft")
+    // This line is used to pass information about the current plug-in.
+    PLUGININFORMATION("AF2igImport",
+                      "Roden Luo",
+                      "2025/04/10",
+                      "Import AF2ig files",
+                      "1.0",
+                      "ProteinCraft")
 
-  // The constructor below has to be defined,
-  // it is the right place to declare the parameters
-  // needed by the algorithm,
-  //   addInParameter<ParameterType>("Name","Help string","Default value");
-  // and declare the algorithm dependencies too.
-  //   addDependency("name", "version");
-  AF2igImport(tlp::PluginContext* context) : ImportModule(context) {
-  }
+    // The constructor below has to be defined,
+    // it is the right place to declare the parameters
+    // needed by the algorithm,
+    //   addInParameter<ParameterType>("Name","Help string","Default value");
+    // and declare the algorithm dependencies too.
+    //   addDependency("name", "version");
+    AF2igImport(tlp::PluginContext* context) : ImportModule(context) {
+    }
 
-  // Define the destructor only if needed
-  // ~MyImportModule() {
-  // }
+    // Define the destructor only if needed
+    // ~MyImportModule() {
+    // }
 
-  // The import method is the starting point of your import module.
-  // The returned value must be true if it succeeded.
-  bool importGraph() override {
-    return true;
-  }
+    // The import method is the starting point of your import module.
+    // The returned value must be true if it succeeded.
+    bool importGraph() override {
+        graph->setName("AF2ig_");
+        return true;
+    }
 };
 // This second line will be used to register your algorithm in tulip using the information given above.
 PLUGIN(AF2igImport)
