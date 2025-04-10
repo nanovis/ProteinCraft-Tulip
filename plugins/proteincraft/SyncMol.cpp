bbfe1c6701d665fec416ad3b00c5f39a60bbe61e
RodenLuo <roden.luo@gmail.com>
2025-04-10 11:57:19 +0300

    Refactor SyncMol class to enhance functionality and integrate network communication
    - Updated SyncMol to inherit from tlp::Algorithm instead of tlp::StringAlgorithm.
    - Added parameters for base URL and base path to facilitate synchronization with ChimeraX.
    - Implemented logic to process AF2ig graph and prepare synchronization data in JSON format.
    - Integrated QNetworkAccessManager for sending requests to ChimeraX and handling responses.
    - Improved error handling for network communication.

--- plugins/proteincraft/SyncMol.cpp
+++ plugins/proteincraft/SyncMol.cpp
@@ -1,5 +1,4 @@
 /**
-*
  * This file is part of ProteinCraft (https://github.com/nanovis/ProteinCraft)
  *
  * Authors: Roden Luo
@@ -7,14 +6,29 @@
  * ProteinCraft builds on top of Tulip.
  *
  */
+#include <QEventLoop>
 #include <tulip/TlpQtTools.h>
 #include <tulip/PropertyAlgorithm.h>
 #include <tulip/BooleanProperty.h>
 #include <tulip/StringProperty.h>
+#include <tulip/Graph.h>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QUrlQuery>
 
 using namespace tlp;
 
-class SyncMol : public tlp::StringAlgorithm {
+static const char *paramHelp[] = {
+    // base url
+    "ChimeraX REST URL (default port 45145)",
+    // base path
+    "Base path for structure files"};
+
+class SyncMol : public tlp::Algorithm {
 
 public:
     PLUGININFORMATION(
@@ -23,16 +37,12 @@ public:
         "1.2", "ProteinCraft")
 
     SyncMol(const tlp::PluginContext *context)
-        // set second parameter of the constructor below to true because
-        // result needs to be an inout parameter
-        // in order to preserve the original values of non targeted elements
-        // i.e if "nodes" == true and "edges==false", the values of edges must be preserved
-        : StringAlgorithm(context, true){
-
+        : Algorithm(context) {
+        addInParameter<std::string>("base url", paramHelp[0], "http://127.0.0.1:45145/run");
+        addInParameter<std::string>("base path", paramHelp[1], "/home/luod/ProteinCraft/run/4_PD-L1/outs_AF2ig_reindex");
     }
 
     std::string category() const override {
-        // return ALGORITHM_CATEGORY;
         return "ProteinCraft";
     }
 
@@ -41,13 +51,101 @@ public:
     }
 
     bool run() override {
+      
+        std::string baseUrl;
+        std::string basePath;
 
+        if (dataSet != nullptr) {
+            dataSet->get("base url", baseUrl);
+            dataSet->get("base path", basePath);
+        }
 
-#ifndef NDEBUG
+        QJsonObject syncData;
 
-        qDebug() << "SyncMol";
+        // Find the required graphs
+        Graph *AF2ig_graph = nullptr;
+        Graph *Tetris_graph = nullptr;
+        Graph *UMAP_graph = nullptr;
 
-#endif // NDEBUG
+        for (Graph *g : tlp::getRootGraphs()) {
+            std::string name = g->getName();
+            if (name.find("AF2ig") == 0) {
+                AF2ig_graph = g;
+            } else if (name.find("Tetris") == 0) {
+                Tetris_graph = g;
+            } else if (name.find("UMAP") == 0) {
+                UMAP_graph = g;
+            }
+        }
+
+        // Process AF2ig graph
+        if (AF2ig_graph) {
+            BooleanProperty *viewSelection = AF2ig_graph->getProperty<BooleanProperty>("viewSelection");
+            StringProperty *description = AF2ig_graph->getProperty<StringProperty>("description");
+
+            for (node n : AF2ig_graph->getNodes()) {
+                if (viewSelection->getNodeValue(n)) {
+                    std::string filename = description->getNodeValue(n);
+                    std::string fullPath = basePath + "/" + filename + ".pdb";
+                    std::string nodeId = std::to_string(n.id);
+
+                    QJsonObject nodeData;
+                    nodeData["id"] = QString::fromStdString(nodeId);
+                    nodeData["name"] = QString::fromStdString(filename);
+                    nodeData["display"] = true;
+
+                    syncData[QString::fromStdString(fullPath)] = nodeData;
+                }
+            }
+        }
+
+        // // Process Tetris graph
+        // if (Tetris_graph) {
+        //     BooleanProperty *viewSelection = Tetris_graph->getProperty<BooleanProperty>("viewSelection");
+        //     StringProperty *description = Tetris_graph->getProperty<StringProperty>("description");
+        //
+        //     for (node n : Tetris_graph->getNodes()) {
+        //         if (viewSelection->getNodeValue(n)) {
+        //             std::string filename = description->getNodeValue(n);
+        //             std::string fullPath = basePath + "/" + filename + ".pdb";
+        //             std::string nodeId = std::to_string(n.id);
+        //
+        //             QJsonObject nodeData;
+        //             nodeData["id"] = QString::fromStdString(nodeId);
+        //             nodeData["name"] = QString::fromStdString(filename);
+        //             nodeData["display"] = true;
+        //
+        //             syncData[QString::fromStdString(fullPath)] = nodeData;
+        //         }
+        //     }
+        // }
+
+        // Convert sync data to JSON string
+        QJsonDocument doc(syncData);
+        QString jsonString = doc.toJson(QJsonDocument::Compact);
+        QString command = QString("proteincraft sync_mol jsonString '%1'").arg(jsonString);
+
+        // Send request to ChimeraX
+        QNetworkAccessManager manager;
+        QUrl url(QString::fromStdString(baseUrl));
+        QUrlQuery query;
+        query.addQueryItem("command", command);
+        url.setQuery(query);
+
+        QNetworkRequest request(url);
+        QNetworkReply *reply = manager.get(request);
+
+        // Wait for the reply
+        QEventLoop loop;
+        QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        loop.exec();
+
+        if (reply->error() != QNetworkReply::NoError) {
+            if (pluginProgress) {
+                pluginProgress->setError(QString("Failed to connect to ChimeraX: %1").arg(reply->errorString()).toStdString());
+            }
+            return false;
+        }
 
         return true;
     }
