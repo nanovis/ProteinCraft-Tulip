46158e8f1055a66d027c82c1931b6e4c64a2bc39
RodenLuo <roden.luo@gmail.com>
2025-04-11 13:32:23 +0300

    Update BipartiteLayout to use subgraph name parameter for layout application
    - Changed the parameter for specifying the subgraph name from a boolean for VDW interactions to a string for the subgraph name, enhancing flexibility in layout application.
    - Updated logic to retrieve and apply the bipartite layout to the specified subgraph, improving usability and error handling.

--- plugins/proteincraft/layout/BipartiteLayout.cpp
+++ plugins/proteincraft/layout/BipartiteLayout.cpp
@@ -9,11 +9,11 @@ using namespace tlp;
 class BipartiteLayout : public tlp::Algorithm {
 public:
   PLUGININFORMATION("BipartiteLayout", "Roden Luo", "2025-03-21",
-                    "Creates a subgraph for chain A-B interactions and sets bipartite layout",
+                    "Applies bipartite layout to a subgraph of chain A-B interactions",
                     "1.0", "ProteinCraft")
 
   BipartiteLayout(tlp::PluginContext *context) : tlp::Algorithm(context) {
-    addInParameter<bool>("include_vdw", "Include VDW interactions in the subgraph?", "True");
+    addInParameter<string>("subgraph_name", "Name of the subgraph to layout", "ChainInteractionSubgraph");
   }
 
   bool check() {
@@ -22,134 +22,42 @@ public:
 
   bool run() override {
     // Retrieve user parameter
-    bool include_vdw = false;
-    dataSet->get("include_vdw", include_vdw);
+    string subgraph_name = "ChainInteractionSubgraph";
+    dataSet->get("subgraph_name", subgraph_name);
 
-    // Get properties from graph
-    StringProperty *prop_chain = graph->getProperty<StringProperty>("chain");
-    IntegerProperty *prop_position = graph->getProperty<IntegerProperty>("position");
-    StringProperty *prop_interaction = graph->getProperty<StringProperty>("interaction");
-
-    // Separate chain A (binder) vs chain B (target)
-    vector<node> binder_nodes;
-    vector<node> target_nodes;
-
-    for (auto n : graph->getNodes()) {
-      string c = prop_chain->getNodeValue(n);
-      if (c == "A") {  // binder
-        binder_nodes.push_back(n);
-      } else if (c == "B") {  // target
-        target_nodes.push_back(n);
+    // Get the subgraph
+    Graph *subgraph = graph->getSubGraph(subgraph_name);
+    if (subgraph == nullptr) {
+      if (pluginProgress) {
+        pluginProgress->setError("Subgraph '" + subgraph_name + "' not found. Please run ChainInteractionSubgraph first.");
       }
+      return false;
     }
 
-    // Sort them by residue number
-    sort(binder_nodes.begin(), binder_nodes.end(), 
-         [&](const node &n1, const node &n2) {
-           return prop_position->getNodeValue(n1) < prop_position->getNodeValue(n2);
-         });
-    sort(target_nodes.begin(), target_nodes.end(),
-         [&](const node &n1, const node &n2) {
-           return prop_position->getNodeValue(n1) < prop_position->getNodeValue(n2);
-         });
-
-    // Identify chain B subset that interacts with chain A
-    set<node> interacting_binder_set;
-    set<node> interacting_target_set;
-
-    for (auto e : graph->getEdges()) {
-      string inter_type = prop_interaction->getEdgeValue(e);
-      if (!is_interesting_interaction(inter_type, include_vdw)) {
-        continue;
+    // Get the stored node lists
+    vector<node> interacting_binder_list;
+    vector<node> interacting_target_list;
+    
+    if (!subgraph->getAttribute("interacting_binder_list", interacting_binder_list) ||
+        !subgraph->getAttribute("interacting_target_list", interacting_target_list)) {
+      if (pluginProgress) {
+        pluginProgress->setError("Subgraph '" + subgraph_name + "' does not contain required node lists. Please run ChainInteractionSubgraph first.");
       }
-
-      node n1 = graph->source(e);
-      node n2 = graph->target(e);
-      string c1 = prop_chain->getNodeValue(n1);
-      string c2 = prop_chain->getNodeValue(n2);
-
-      if (c1 == "A" && c2 == "B") {
-        interacting_binder_set.insert(n1);
-        interacting_target_set.insert(n2);
-      } else if (c1 == "B" && c2 == "A") {
-        interacting_binder_set.insert(n2);
-        interacting_target_set.insert(n1);
-      }
-    }
-
-    vector<node> interacting_binder_list(interacting_binder_set.begin(), interacting_binder_set.end());
-    vector<node> interacting_target_list(interacting_target_set.begin(), interacting_target_set.end());
-
-    sort(interacting_binder_list.begin(), interacting_binder_list.end(),
-         [&](const node &n1, const node &n2) {
-           return prop_position->getNodeValue(n1) < prop_position->getNodeValue(n2);
-         });
-    sort(interacting_target_list.begin(), interacting_target_list.end(),
-         [&](const node &n1, const node &n2) {
-           return prop_position->getNodeValue(n1) < prop_position->getNodeValue(n2);
-         });
-
-    // Create or reset subgraph
-    Graph *binder_target_sub = graph->getSubGraph("BinderTargetInteraction");
-    if (binder_target_sub == nullptr) {
-      binder_target_sub = graph->addSubGraph("BinderTargetInteraction");
-    } else {
-      // clear existing content
-      for (auto n : binder_target_sub->getNodes()) {
-        binder_target_sub->delNode(n);
-      }
-      for (auto e : binder_target_sub->getEdges()) {
-        binder_target_sub->delEdge(e);
-      }
-    }
-
-    // Add nodes
-    set<node> binder_target_nodes(interacting_binder_list.begin(), interacting_binder_list.end());
-    binder_target_nodes.insert(interacting_target_list.begin(), interacting_target_list.end());
-    for (auto n : binder_target_nodes) {
-      binder_target_sub->addNode(n);
-    }
-
-    // Add edges - only inter-chain interactions
-    for (auto e : graph->getEdges()) {
-      node s = graph->source(e);
-      node t = graph->target(e);
-      if (binder_target_nodes.find(s) != binder_target_nodes.end() &&
-          binder_target_nodes.find(t) != binder_target_nodes.end()) {
-        string c1 = prop_chain->getNodeValue(s);
-        string c2 = prop_chain->getNodeValue(t);
-        // Only add edges between different chains
-        if ((c1 == "A" && c2 == "B") || (c1 == "B" && c2 == "A")) {
-          string inter_type = prop_interaction->getEdgeValue(e);
-          if (is_interesting_interaction(inter_type, include_vdw)) {
-            binder_target_sub->addEdge(e);
-          }
-        }
-      }
+      return false;
     }
 
     // Layout the subgraph
-    LayoutProperty *sub_view_layout = binder_target_sub->getLocalProperty<LayoutProperty>("viewLayout");
-    layout_bipartite_subgraph(graph, interacting_binder_list, interacting_target_list, sub_view_layout);
+    LayoutProperty *sub_view_layout = subgraph->getLocalProperty<LayoutProperty>("viewLayout");
+    layout_bipartite_subgraph(subgraph, interacting_binder_list, interacting_target_list, sub_view_layout);
 
     if (pluginProgress) {
-      pluginProgress->setComment("Created subgraph 'BinderTargetInteraction' with bipartite layout for chain A/B interactions.");
+      pluginProgress->setComment("Applied bipartite layout to subgraph '" + subgraph_name + "'.");
     }
 
     return true;
   }
 
 private:
-  bool is_interesting_interaction(const string &inter_type, bool include_vdw) {
-    if (inter_type.empty()) {
-      return false;
-    }
-    if (inter_type.find("VDW") == 0) {
-      return include_vdw;
-    }
-    return true;
-  }
-
   void layout_bipartite_subgraph(Graph *graph, const vector<node> &interacting_binder_list,
                                 const vector<node> &interacting_target_list,
                                 LayoutProperty *sub_view_layout) {
